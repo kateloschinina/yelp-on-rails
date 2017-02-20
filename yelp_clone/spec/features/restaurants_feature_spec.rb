1f3cfa75a6a90ff71b7fb6f744e92d37a241a940
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-5.local>
2017-02-20 17:04:48 +0000

    Completed V1 Section 7.

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add new restaurant'
     end
   end
+
+  context 'restaurant have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
 end
